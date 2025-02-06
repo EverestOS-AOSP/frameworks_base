ij-project/android_frameworks_base_new

Whitelist launcher widgets from bitmap memory usage restriction
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

services/appwidget/java/com/android/server/appwidget/AppWidgetServiceImpl.java
@@ -2483,7 +2483,8 @@ class AppWidgetServiceImpl extends IAppWidgetService.Stub implements WidgetBacku
             int memoryUsage;
             if ((UserHandle.getAppId(Binder.getCallingUid()) != Process.SYSTEM_UID) &&
                     (widget.views != null) &&
-                    ((memoryUsage = widget.views.estimateMemoryUsage()) > mMaxWidgetBitmapMemory)) {
+                    ((memoryUsage = widget.views.estimateMemoryUsage()) > mMaxWidgetBitmapMemory)
+                    && !com.android.internal.util.android.BypassUtils.isSystemLauncher(Binder.getCallingUid())) {
                 widget.views = null;
                 throw new IllegalArgumentException("RemoteViews for widget update exceeds"
                         + " maximum bitmap memory usage (used: " + memoryUsage

EverestOS-AOSP/frameworks_base

Change-Id: I74f6d4da59029d2f0cde19d97b797c539870adaf

services/appwidget/java/com/android/server/appwidget/AppWidgetServiceImpl.java
@@ -2483,7 +2483,8 @@ class AppWidgetServiceImpl extends IAppWidgetService.Stub implements WidgetBacku
             int memoryUsage;
             if ((UserHandle.getAppId(Binder.getCallingUid()) != Process.SYSTEM_UID) &&
                     (widget.views != null) &&
-                    ((memoryUsage = widget.views.estimateMemoryUsage()) > mMaxWidgetBitmapMemory)) {
+                    ((memoryUsage = widget.views.estimateMemoryUsage()) > mMaxWidgetBitmapMemory)
+                    && !com.android.internal.util.custom.BypassUtils.isSystemLauncher(Binder.getCallingUid())) {
                 widget.views = null;
                 throw new IllegalArgumentException("RemoteViews for widget update exceeds"
                         + " maximum bitmap memory usage (used: " + memoryUsage